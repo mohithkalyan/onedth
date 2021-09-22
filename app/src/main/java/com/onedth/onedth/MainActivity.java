ce1b2c8d2d24e5600dc1d768b511874b9fae0dd2
mohith <pmohithkalyan@gmail.com>
2021-09-22 21:26:29 +0530

    first commit

---
+++ app/src/main/java/com/onedth/onedth/MainActivity.java
@@ -0,0 +1,29 @@
+package com.onedth.onedth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.onedth.onedth.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/com/onedth/onedth/RechargeType.java
@@ -0,0 +1,14 @@
+package com.onedth.onedth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RechargeType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recharge_type);
+    }
+}
